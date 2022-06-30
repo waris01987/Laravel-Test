700760a299dbb567825f3a771f74db1c6ceea5fc
Waris Ali <warisali01987@gmail.com>
2022-06-30 20:45:19 +0530

    Complete the getMenuItems function

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -94,6 +94,7 @@ class MenuController extends BaseController
      */
 
     public function getMenuItems() {
-        throw new \Exception('implement in coding task 3');
+        $data = MenuItem::whereNull('parent_id')->with('children')->get()->toArray();
+        return response()->json($data, 200);
     }
 }
--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -8,5 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class MenuItem extends Model
 {
+	/**
+     * Get sub menus for the current menu.
+     */
+	public function subMenus() {
+	    return $this->hasMany($this, 'parent_id', 'id');
+	}
 
+	/**
+     * Get all childrens (recursive call) for the current menu.
+     */
+	public function children() {
+	    return $this->subMenus()->with('children');
+	}
 }
